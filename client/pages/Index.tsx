2ab0546a2a4e60fc56f2f10a7b93a0e82a8e14d0
Ishwar78 <ishwarsharma27092002@gmail.com>
2025-07-31 10:19:06 +0530

    ok

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -265,11 +265,11 @@ export default function Index() {
         </div>
 
         {/* Scroll indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+        {/* <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
           <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
             <div className="w-1 h-3 bg-white rounded-full mt-2 animate-pulse"></div>
           </div>
-        </div>
+        </div> */}
       </section>
 
       {/* About Us Section */}
